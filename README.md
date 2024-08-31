7cd681a85f1cb0b86784f57cdabf906719d43963
Lucas Conceição <lucas.conceicao@ftdata.com.br>
2024-08-31 20:31:09 -0300

    subindo tudo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projeto Web em VUE consumindo de uma Api Marvel
+
+### Inicie o projeto com este comando
+
+```sh
+npm run dev
+```
---
+++ src/components/CardView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { defineProps } from "vue";
+import image from "../assets/no-image.jpg";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  urlImage: {
+    default: null,
+  },
+  extension: {
+    default: "jpg",
+  },
+  id: {
+    default: 0,
+  },
+});
+
+const validImg = (url = null, extension = "jpg") => {
+  if (url && typeof url == "string" && url.length > 0) {
+    return url + "." + extension;
+  }
+
+  return image;
+};
+</script>
+
+<template>
+  <div
+    class="w-96 h-96 m-2 border rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 cursor-pointer hover:bg-customGray"
+  >
+    <div class="flex flex-col items-center">
+      <img
+        style="height: 21rem; width: 21rem"
+        class="max-w-full min-w-full shadow-xl"
+        :src="validImg(props.urlImage, props.extension)"
+        :alt="props.title"
+      />
+
+      <span class="py-3"> {{ props.title }}</span>
+    </div>
+  </div>
+</template>
